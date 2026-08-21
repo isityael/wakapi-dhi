c4866e327201e7fabc5fe00927ad5c7d97f832eb
Yael Meya <yael@m0sh1.cc>
2026-08-21 12:50:43 +0200

    fix(auth): preserve additional API key access scope

--- services/user.go
+++ services/user.go
@@ -107,7 +107,7 @@ func (srv *UserService) GetUserByKey(key string, requireFullAccessKey bool) (*mo
 		return nil, errors.New("key must not be empty")
 	}
 
-	cacheKey := fmt.Sprintf("key_%s", key)
+	cacheKey := fmt.Sprintf("key_%t_%s", requireFullAccessKey, key)
 	if u, ok := srv.cache.Get(cacheKey); ok {
 		return u.(*models.User), nil
 	}
